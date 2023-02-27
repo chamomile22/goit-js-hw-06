6e309b45b694c452b5b527e4e0aecd81cfcb3832
Yana Babak <yana.i.babak@gmail.com>
2023-02-27 20:17:02 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const ingredientsItems = ingredients.map((ing) => {
+	// 1
+	const ingredient = document.createElement("li");
+	// 2
+	ingredient.textContent = ing;
+	// 3
+	ingredient.classList.add(".item");
+	return ingredient;
+});
+
+// 4
+const ingredientsList = document.querySelector("#ingredients");
+ingredientsList.append(...ingredientsItems);
