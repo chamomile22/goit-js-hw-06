dfd9501f66c8038ab199832c3bae4b8669cc512e
Yana Babak <yana.i.babak@gmail.com>
2023-02-28 22:56:10 +0200

    task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+const decr = document.querySelector("button[data-action='decrement']");
+const incr = document.querySelector("button[data-action='increment']");
+const value = document.querySelector("#value");
+let counterValue = 0;
+const handelClickMinus = (event) => {
+	event.preventDefault();
+	counterValue -= 1;
+	value.innerHTML = `<span>${counterValue}</span>`;
+};
+const handelClickPlus = (event) => {
+	event.preventDefault();
+	counterValue += 1;
+	value.innerHTML = `<span>${counterValue}</span>`;
+};
+
+decr.addEventListener("click", handelClickMinus);
+incr.addEventListener("click", handelClickPlus);
