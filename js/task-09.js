ab27a223e657cf3ed5e43084280db13600ede3ba
Yana Babak <yana.i.babak@gmail.com>
2023-03-03 22:19:18 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,20 @@
+"use strict";
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const color = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+const handleColorChanging = (event) => {
+	event.preventDefault();
+	let gottenColor = getRandomHexColor();
+	color.textContent = `${gottenColor}`;
+	body.style.backgroundColor = gottenColor;
+};
+
+btnChangeColor.addEventListener("click", handleColorChanging);
