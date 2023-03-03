1d2d3d78542c54ab22a7bcfb455078385fd3cf76
Yana Babak <yana.i.babak@gmail.com>
2023-03-03 21:12:33 +0200

    task 6 done

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const textInput = document.querySelector('[data-length = "6"]');
+
+const handleInputValidation = (event) => {
+	if (textInput.value.length >= textInput.dataset.length) {
+		textInput.classList.add("valid");
+		textInput.classList.remove("invalid");
+	} else {
+		textInput.classList.remove("valid");
+		textInput.classList.add("invalid");
+	}
+};
+
+textInput.addEventListener("blur", handleInputValidation);
