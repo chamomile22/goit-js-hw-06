a32d8854da51e5f80ae5ea1893a9296b47ca6698
Yana Babak <yana.i.babak@gmail.com>
2023-03-03 22:08:30 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+"use strict";
 
+const form = document.querySelector(".login-form");
+
+const handleFormFilling = (event) => {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === "" || password.value === "") {
+		alert("Усі поля форми повинні бути заповнені");
+	}
+
+	const formSubmitInfo = {
+		[email.name]: email.value,
+		[password.name]: password.value,
+	};
+	console.log(formSubmitInfo);
+
+	event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handleFormFilling);
